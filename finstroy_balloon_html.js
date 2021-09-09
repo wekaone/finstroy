b677775ad3c7a76447d196a0e76d99a031d10f08
wekaone <47196099+wekaone@users.noreply.github.com>
2021-09-09 16:50:39 +0300

    Update finstroy_balloon_html.js

--- finstroy_balloon_html.js
+++ finstroy_balloon_html.js
@@ -8,17 +8,16 @@ function init() {
             searchControlProvider: 'yandex#search'
         });
     myMap.geoObjects
-        .add(new ymaps.Placemark([53.30704688152192,50.28914832598631], {
+        .add(new ymaps.Placemark([53.31005907115121,50.28758199999998], {
             // Зададим содержимое заголовка балуна.
-            balloonContentHeader: '<a href = "http://finstroy.tilda.ws/object" target="_blank">Офис отдела продаж и аренды<br>ЖК "Новая Самара"</a><br>' +
-            '<span class="description">ООО "СЗ "ФИНСТРОЙ"<br><br><i>Режим работы:</i><br>пн-пт с 09 до 19<br>сб-вс с 09 до 15<br><br><i>Адрес:</i><br><a href="https://yandex.ru/maps/51/samara/?ll=50.289406%2C53.307253&mode=poi&poi%5Bpoint%5D=50.289373%2C53.307363&poi%5Buri%5D=ymapsbm1%3A%2F%2Forg%3Foid%3D2999599358&z=21">г. Самара, Красноглинский р-н, <br>поселок Мехзавод 1- й квартал, д. 40</a></span>',
+            balloonContentHeader: '<a href = "http://finstroy.tilda.ws/object" target="_blank">ЖК «Новая Самара»</a><br>' +
+            '<span class="description">г. Самара, пос. Мехзавод, Квартал 1, Дом 33</span>',
             // Зададим содержимое основной части балуна.
-            balloonContentBody: '<img src="https://static.tildacdn.com/tild3131-3535-4731-b633-653238653735/office.jpg" height="150" width="300" title="Новая Самара" alt="ЖК Новая Самара"> <br/> ' +           
-            '<b><a href = "#popup:zvonok" target="_blank" style="font-size: 14px">➥ <u>Заказать обратный звонок</u></a></b>',
+            balloonContentBody: '<a href = "#popup:zvonok" target="_blank" style="font-size: 14px">➥ <u>Смотреть подробнее</u></a></b>',
             // Зададим содержимое нижней части балуна.
-            balloonContentFooter: '',
+            balloonContentFooter: 'УК «Финстрой»',
             // Зададим содержимое всплывающей подсказки.
-            hintContent: 'Офис продаж ЖК "Новая Самара"'
+            hintContent: 'г. Самара, пос. Мехзавод, Квартал 1, Дом 33'
             }, {
                 // Опции.
                 // Необходимо указать данный тип макета.
